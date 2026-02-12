c6398662478ad76afa91acdd96671d94520c555b
Ziery-Dev <zierydev@gmail.com>
2026-02-12 14:53:06 -0300

    fix: ajuste baseUrl axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ import { useAuthStore } from '@/stores/auth'
 import { notify } from './notify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL:  process.env.VUE_APP_API_URL || "http://localhost:8080",
   withCredentials: true
 });
 
